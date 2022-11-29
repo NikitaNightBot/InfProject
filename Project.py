bc55b98144630a688083ee2b69c0daa334b27c6f
NikitaNightBot <41861454+NikitaNightBot@users.noreply.github.com>
2022-11-29 18:33:42 +0300

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,9 +5,11 @@ import pyperclip
 def main():
     match str(input("Input:String/File\n")).lower():
         case "file":
-            pyperclip.copy(getattr(hashlib, input(f'{hashlib.algorithms_available}\n'))(open(f'{(str(input("Enter file path: ")))}', 'r').read().encode('utf-8')).hexdigest())
+            pyperclip.copy(getattr(hashlib, input(f'{hashlib.algorithms_available}\n'))(
+                open(f'{(str(input("Enter file path: ")))}', 'r').read().encode('utf-8')).hexdigest())
         case "string":
-            pyperclip.copy(getattr(hashlib, input(f'{hashlib.algorithms_available}\n'))(str(input("Enter string: ")).encode('utf-8')).hexdigest())
+            pyperclip.copy(getattr(hashlib, input(f'{hashlib.algorithms_available}\n'))(
+                str(input("Enter string: ")).encode('utf-8')).hexdigest())
     print("Hash succesfully copied to clipboard.")
 
 
